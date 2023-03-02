df6c706d020c16d7f39b4eda9b4b513cf80821a8
Curtis Rueden <ctrueden@wisc.edu>
2023-03-02 15:49:36 -0600

    Happy New Year 2023

--- src/main/java/org/scijava/config/logback/StdOutFilter.java
+++ src/main/java/org/scijava/config/logback/StdOutFilter.java
@@ -2,7 +2,7 @@
  * #%L
  * SciJava Configuration bundle for SciJava software.
  * %%
- * Copyright (C) 2014 - 2022 SciJava developers.
+ * Copyright (C) 2014 - 2023 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
